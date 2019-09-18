7a07cfedfd34170cc2e38c4d4a36c4b6c89dc3ab
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-09-18 14:07:25 -0300

    Converção functional component para class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
